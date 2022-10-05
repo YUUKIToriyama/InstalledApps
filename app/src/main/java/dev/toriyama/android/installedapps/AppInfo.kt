6415ceeb93443721145c5c13fc19895b3b08bbb2
YUUKIToriyama <github@toriyama.dev>
2022-10-05 20:10:32 +0900

    Adapterに渡すためのdata classを作成

---
+++ app/src/main/java/dev/toriyama/android/installedapps/AppInfo.kt
@@ -0,0 +1,9 @@
+package dev.toriyama.android.installedapps
+
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val name: String,
+    val version: String,
+    val logo: Drawable
+)
