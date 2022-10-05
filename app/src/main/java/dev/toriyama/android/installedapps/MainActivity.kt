bd578e9b39b83318a30d41fd6fd50789ed4eb017
YUUKIToriyama <github@toriyama.dev>
2022-10-05 19:01:40 +0900

    端末にインストールされたアプリの名称をログに出力する

--- app/src/main/java/dev/toriyama/android/installedapps/MainActivity.kt
+++ app/src/main/java/dev/toriyama/android/installedapps/MainActivity.kt
@@ -1,5 +1,7 @@
 package dev.toriyama.android.installedapps
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showPackages()
+    }
+
+    private fun showPackages() {
+        val packageManager = applicationContext.packageManager
+        val packages: List<PackageInfo> = packageManager.getInstalledPackages(PackageManager.GET_META_DATA)
+        packages.forEach {
+            val name = it.packageName
+            println(name)
+        }
     }
 }
