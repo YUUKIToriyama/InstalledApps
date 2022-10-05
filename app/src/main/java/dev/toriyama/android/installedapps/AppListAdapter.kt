44711a1c39fa5d17bd225c9f29cc8aea7f3810fc
YUUKIToriyama <github@toriyama.dev>
2022-10-05 20:11:08 +0900

    リサイクラービューのAdapter,ViewHolderを作成

---
+++ app/src/main/java/dev/toriyama/android/installedapps/AppListAdapter.kt
@@ -0,0 +1,33 @@
+package dev.toriyama.android.installedapps
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import dev.toriyama.android.installedapps.databinding.ListApplicationBinding
+
+class AppListAdapter(
+    context: Context,
+    private val appList: List<AppInfo>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return AppListViewHolder(
+            ListApplicationBinding.inflate(inflater, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is AppListViewHolder) {
+            val item = appList[position]
+            holder.binding.imageviewAppLogo.setImageDrawable(item.logo)
+            holder.binding.textAppName.text = item.name
+            holder.binding.textAppVersion.text = item.version
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return appList.size
+    }
+}
---
+++ app/src/main/java/dev/toriyama/android/installedapps/AppListViewHolder.kt
@@ -0,0 +1,8 @@
+package dev.toriyama.android.installedapps
+
+import androidx.recyclerview.widget.RecyclerView
+import dev.toriyama.android.installedapps.databinding.ListApplicationBinding
+
+class AppListViewHolder(
+    val binding: ListApplicationBinding
+) : RecyclerView.ViewHolder(binding.root)
